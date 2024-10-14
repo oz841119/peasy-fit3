7ec011798b3d3667f223347e01844acc1e949100
Yili <oz841119@gmail.com>
2024-10-14 14:00:07 +0800

    develop

--- packages/db/prisma/migrations/20241014053838_init/migration.sql
+++ packages/db/prisma/migrations/20241014053838_init/migration.sql
@@ -1,4 +1,14 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Training" (
     "id" SERIAL NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
@@ -9,3 +19,6 @@ CREATE TABLE "Training" (
 
     CONSTRAINT "Training_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
