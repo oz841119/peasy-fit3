e054edaa8dfafbdc4989aa1c82df5058a5250094
oz841119 <oz841119@gmail.com>
2024-11-17 01:40:58 +0800

    fix: issues of setstate-in-render

--- apps/web/app/[lang]/dashboard/layout.tsx
+++ apps/web/app/[lang]/dashboard/layout.tsx
@@ -4,15 +4,16 @@ import { TopBar } from "@/components/Bars/TopBar/TopBar";
 import { Toaster } from "@/components/ui/toaster";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
   const { data, status } = useSession()
   const router = useRouter()
-  if(status !== 'loading' && !data) {
-    router.push('/login')
-    return
-  }
+  useEffect(() => {
+    if(status !== 'loading' && !data) {
+      router.push('/login')
+    }
+  }, [status, data, router])
   return (
     <div className="min-h-screen bg-muted/50 flex text-muted-foreground">
       <SideBar/>
