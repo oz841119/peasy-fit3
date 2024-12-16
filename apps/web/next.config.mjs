08e4a9198795e0895cd36fb9941ac01d4b8dd296
Yili <oz841119@gmail.com>
2024-12-16 11:40:02 +0800

    test: Vercel deploy test

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,16 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
+
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
+};
 
 export default withNextIntl(nextConfig);
