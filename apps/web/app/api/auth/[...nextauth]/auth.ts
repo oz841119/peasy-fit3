6df0d4659379479853328fc777dac4a30ac54829
Yili <oz841119@gmail.com>
2024-12-19 13:41:55 +0800

    feat: Ensure that the user can select an account when using Google Login.

--- apps/web/app/api/auth/[...nextauth]/auth.ts
+++ apps/web/app/api/auth/[...nextauth]/auth.ts
@@ -41,6 +41,13 @@ const getProviders = () => {
       GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code"
+          }
+        }
       })
     );
   }
