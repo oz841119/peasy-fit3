dd7f453c643e627e16e652f26d77ccf78b250723
Yili <oz841119@gmail.com>
2025-01-10 14:06:45 +0800

    chore: jest

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  // collectCoverage: true,
+  // coverageDirectory: "coverage",
+  preset: 'ts-jest/presets/default-esm',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
+
+export default config;
