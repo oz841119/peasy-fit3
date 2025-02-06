0091373145108609f4a263721be96b4995d69a83
oz841119 <oz841119@gmail.com>
2025-02-07 02:05:22 +0800

    WIP: session

--- apps/web/components/Bars/TopBar/TopBar.tsx
+++ apps/web/components/Bars/TopBar/TopBar.tsx
@@ -4,10 +4,12 @@ import { NavBreadcrumb } from "./NavBreadcrumb/NavBreadcrumb";
 import { User } from "./User/User";
 import { MAX_MOBILE_SIZE } from "@/constants";
 import { MobileMenu } from "./MobileMenu/MobileMenu";
-import { usePathname } from "@/i18n/routing";
+import { Link, usePathname } from "@/i18n/routing";
 import { menu } from "@/constants/menu";
 import { useMemo } from "react";
 import { useTranslations } from "next-intl";
+import { Dumbbell } from "lucide-react";
+import { Tooltip } from "@/components/ui/Tooltip";
 export function TopBar() {
   const windowSize = useWindowSize()
   const pathname = usePathname()
@@ -16,18 +18,26 @@ export function TopBar() {
     const routeItem = menu.find(item => item.route === pathname)
     return routeItem?.label
   }, [pathname])
+  const isInTraining = true;
   return (
     <div className="py-4 flex items-center">
       {/* <NavBreadcrumb></NavBreadcrumb> */}
       <div className="flex gap-2 items-end">
-        { windowSize.width <= MAX_MOBILE_SIZE && <h1 className="text-foreground font-bold text-xl">Peasy Fit</h1>}
-        <div className="text-sm">{ pathLabel ? t(pathLabel) : 'Unknown' }</div>
+        {windowSize.width <= MAX_MOBILE_SIZE && <h1 className="text-foreground font-bold text-xl">Peasy Fit</h1>}
+        <div className="text-sm">{pathLabel ? t(pathLabel) : 'Unknown'}</div>
       </div>
-      {
-        windowSize.width > MAX_MOBILE_SIZE 
-          ? <User triggerClassName="ml-auto"></User> 
-          : <MobileMenu triggerClassName="ml-auto"/>
+      <div className="ml-auto flex gap-2 items-center">
+        <Tooltip text={isInTraining ? t('common.inTraining') : t('common.notInTraining')}>
+          <Link href="/dashboard/session">
+            <Dumbbell color={isInTraining ? 'green' : 'red'} size="18" />
+          </Link>
+        </Tooltip>
+        {
+          windowSize.width > MAX_MOBILE_SIZE
+            ? <User triggerClassName="ml-auto"></User>
+            : <MobileMenu triggerClassName="ml-auto" />
         }
+      </div>
     </div>
   )
 }
---
+++ apps/web/components/ui/Tooltip.tsx
@@ -0,0 +1,16 @@
+import { Tooltip as ShadcnUITooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../shadcnUI/tooltip";
+
+export function Tooltip({ children, text }: React.PropsWithChildren & { text: string }) {
+  return (
+    <TooltipProvider delayDuration={0}>
+      <ShadcnUITooltip>
+        <TooltipTrigger asChild>
+          { children }
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{ text }</p>
+        </TooltipContent>
+      </ShadcnUITooltip>
+    </TooltipProvider>
+  )
+}
