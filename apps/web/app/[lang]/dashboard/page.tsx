dc0187356778e4ee7c80eda4f0c772bfe10357fd
Yili <oz841119@gmail.com>
2025-04-21 17:09:32 +0800

    feat: Add a full calendar on the homepage to provide a more intuitive overview of the records.

--- apps/web/app/[lang]/dashboard/page.tsx
+++ apps/web/app/[lang]/dashboard/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+import { FullCalendar } from "@/components/FullCalendar/FullCalendar";
+
 export default function DashboardPage() {
-	return <div>^^</div>;
+	const events = [
+		{
+			id: 1,
+			title: "Event 1",
+			date: new Date(),
+		},
+		{
+			id: 2,
+			title: "Event 2",
+			date: new Date(),
+		},
+		{
+			id: 3,
+			title: "Event 3",
+			date: new Date(),
+		},
+		{
+			id: 4,
+			title: "Event 4",
+			date: new Date(Date.now() - 24 * 60 * 60 * 1000),
+		},
+		{
+			id: 5,
+			title: "Event 5",
+			date: new Date(Date.now() - 24 * 60 * 60 * 1000),
+		},
+		{
+			id: 6,
+			title: "Event 6",
+			date: new Date(Date.now() - 24 * 60 * 60 * 1000),
+		},
+	];
+	const handleEventClick = (event: typeof events[number]) => {
+		console.log(event);
+	};
+	return (
+		<div>
+			<FullCalendar events={events} onEventClick={handleEventClick} />
+		</div>
+	);
 }
--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -2,16 +2,10 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 export function capitalizeFirstLetter(str: string) {
-	if (!str) return "";
-	return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-export function getPreferredLocaleByAcceptLanguage(acceptLanguage: string) {
-	const languages = acceptLanguage.split(",");
-	const preferredLanguage = languages[0];
-	return preferredLanguage;
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
