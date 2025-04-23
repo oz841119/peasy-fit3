a46a3bcf3e2b5f25c2238be0fd804dcea7514676
oz841119 <oz841119@gmail.com>
2025-04-23 22:31:22 +0800

    feat: enable CORS only in development

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { log } from "util";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  console.log(process.env.NODE_ENV);
+  const app = await NestFactory.create(AppModule);
+  if (process.env.NODE_ENV === "development") {
+    app.enableCors();
+  }
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.NEST_PORT ?? 3003);
 }
